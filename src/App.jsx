575f72d8bd303bfc14d7652a6a8423e72c60bb73
yonghoon <yongpro0526@gmail.com>
2026-08-26 02:15:12 +0900

    fix: correct contact import path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MainLayout from "./layouts/MainLayout"
 import SidebarLayout from "./layouts/SidebarLayout"
 
 import Home from "./pages/home/Home";
-import Projects from "./pages/projecs/Projects";
+import Projects from "./pages/projects/Projects";
 import Contact from "./pages/contact/Contact";
 import AboutRoutes from "./routes/AboutRoutes";
 import ProjectRoutes from "./routes/ProjectRoutes";
